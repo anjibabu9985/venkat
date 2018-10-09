feec88ee0b9cec5ad4cef0e56e78ea2ae60643db
anjibabu <bandianjibabu8@gmail.com>
2018-10-09 16:20:52 +0530

    Updates

---
+++ precache-manifest.6816b7125c17c83b1fdce95d045d48bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f96223f80a36cc259cb9cfa2ef968ad5",
+    "url": "/venkat/static/media/logo.f96223f8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/venkat/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "16ec5dc00c8c099c9281afce043358ea",
+    "url": "/venkat/static/media/bg.16ec5dc0.jpg"
+  },
+  {
+    "revision": "f2a6d561d7d797ae20c2",
+    "url": "/venkat/static/js/runtime~main.f2a6d561.js"
+  },
+  {
+    "revision": "a06d20b0b75956ed9506",
+    "url": "/venkat/static/js/main.a06d20b0.chunk.js"
+  },
+  {
+    "revision": "448efbf979378e4a2c66",
+    "url": "/venkat/static/js/1.448efbf9.chunk.js"
+  },
+  {
+    "revision": "a06d20b0b75956ed9506",
+    "url": "/venkat/static/css/main.90a720d8.chunk.css"
+  },
+  {
+    "revision": "a855aa637b45059487e5c1b5d5e19a5b",
+    "url": "/venkat/index.html"
+  }
+];
